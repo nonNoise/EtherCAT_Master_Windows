97e5efeab28b056abc0358417d5f97f68f69a64d
kitagami <kitagami@abund.jp>
2021-05-25 17:07:10 +0900

    VisualStadioでリンクした祝い

---
+++ vs2019_project/test01/test01.c
@@ -0,0 +1,31 @@
+﻿
+#include <stdio.h>
+#include <pcap.h>
+
+void main()
+{
+	printf("HELLO\n");
+	pcap_if_t* alldevsp, * device;
+
+
+	char errbuf[100];
+	int count = 1;
+
+	//First get the list of available devices
+	printf("Finding available devices ... \n");
+	if (pcap_findalldevs(&alldevsp, errbuf))
+	{
+		printf("Error finding devices : %s\n\n", errbuf);
+		exit(1);
+	}
+	printf("Done\n");
+
+	//Print the available devices
+	printf("Available Devices are :\n");
+	for (device = alldevsp; device != NULL; device = device->next)
+	{
+		printf("%d. %s - %s\n\n", count, device->name, device->description);
+		count++;
+	}
+	while (1);
+}
